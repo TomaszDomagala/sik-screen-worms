6d27c79952f89429c311b99d4bcb17ceabfea29c
Tomasz Domagała <tomasz.domagala.contact@gmail.com>
2021-05-19 22:28:59 +0200

    Add timeout functionality

--- src/server/clients.c
+++ src/server/clients.c
@@ -2,45 +2,59 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <sys/timerfd.h>
+#include <unistd.h>
 
 
 #define MAX_CLIENTS_NUM 32
 
 struct clients_collection_s {
-	client_t arr[MAX_CLIENTS_NUM];
-	uint32_t num;
+    client_t arr[MAX_CLIENTS_NUM];
+    uint32_t num;
 };
 
 clients_collection_t *clients_create() {
-	clients_collection_t *clients = malloc(sizeof(clients_collection_t));
-	if (clients == NULL) {
-		return NULL;
-	}
-	for (int i = 0; i < MAX_CLIENTS_NUM; ++i) {
-		clients->arr[i].sock_fd = -1;
-		clients->arr[i].timer_fd = -1;
-		clients->arr[i].session_id = 0;
-
-	}
-	clients->num = 0;
-	return clients;
+    clients_collection_t *clients = malloc(sizeof(clients_collection_t));
+    if (clients == NULL) {
+        return NULL;
+    }
+    for (int i = 0; i < MAX_CLIENTS_NUM; ++i) {
+        clients->arr[i].sock_fd = -1;
+    }
+    clients->num = 0;
+    return clients;
 }
 
 client_t *clients_new_client(clients_collection_t *clients, uint64_t session_id, int32_t sock_fd) {
-	assert(clients != NULL);
-	if (clients->num == MAX_CLIENTS_NUM) {
-		return NULL;
-	}
-	int i;
-	for (i = 0; i < MAX_CLIENTS_NUM; ++i) {
-		if (clients->arr[i].sock_fd == -1) {
-			clients->arr[i].session_id = session_id;
-			clients->arr[i].sock_fd = sock_fd;
-			// TODO add timerfd initialization.
-			break;
-		}
-	}
-	assert(i < MAX_CLIENTS_NUM);
-	clients->num++;
-	return &clients->arr[i];
+    assert(clients != NULL);
+    if (clients->num == MAX_CLIENTS_NUM) {
+        return NULL;
+    }
+    int i;
+    for (i = 0; i < MAX_CLIENTS_NUM; ++i) {
+        if (clients->arr[i].sock_fd == -1) {
+            clients->arr[i].session_id = session_id;
+            clients->arr[i].sock_fd = sock_fd;
+            clients->arr[i].timer_fd = timerfd_create(CLOCK_BOOTTIME, 0);
+            break;
+        }
+    }
+    assert(i < MAX_CLIENTS_NUM);
+    clients->num++;
+    return &clients->arr[i];
+}
+
+void clients_delete_client(clients_collection_t *clients, int32_t sock_fd) {
+    assert(clients != NULL);
+
+    for (int i = 0; i < MAX_CLIENTS_NUM; ++i) {
+        client_t *client = &clients->arr[i];
+        if (client->sock_fd == sock_fd) {
+            client->sock_fd = -1;
+
+            close(client->sock_fd);
+            close(client->timer_fd);
+        }
+    }
+
 }
--- src/server/clients.h
+++ src/server/clients.h
@@ -8,17 +8,38 @@ struct clients_collection_s;
 typedef struct clients_collection_s clients_collection_t;
 
 typedef struct {
-	uint64_t session_id;
-	int32_t sock_fd;
-	int32_t timer_fd;
+    uint64_t session_id;
+    int32_t sock_fd;
+    int32_t timer_fd;
 } client_t;
 
+/**
+ * Create new clients collection.
+ */
 clients_collection_t *clients_create();
 
+/**
+ * Free clients collection.
+ * @param clients
+ */
 void clients_free(clients_collection_t *clients);
 
+/**
+ * Create new client with session_id and sock_fd.
+ * New timerfd is created for this client.
+ * @param clients - pointer to the clients collection.
+ * @param session_id - client's session id.
+ * @param sock_fd - client's socket file descriptor.
+ * @return pointer to the created client or NULL if client could not be created.
+ */
 client_t *clients_new_client(clients_collection_t *clients, uint64_t session_id, int32_t sock_fd);
 
+/**
+ * Delete client with given sock_fd and close
+ * file descriptors associated with this client.
+ * @param clients - pointer to the clients collection.
+ * @param sock_fd - client's socket file descriptor.
+ */
 void clients_delete_client(clients_collection_t *clients, int32_t sock_fd);
 
 #endif //SIK_SCREEN_WORMS_CLIENTS_H
