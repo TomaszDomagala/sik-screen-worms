ce24218283aa8ad6ac0f16df8aa24d350a3e3bae
Tomasz Domagała <tomasz.domagala.contact@gmail.com>
2021-05-26 20:20:23 +0200

    WIP

---
+++ src/common/utils/utils.c
@@ -0,0 +1,5 @@
+//
+// Created by tom on 26.05.2021.
+//
+
+#include "utils.h"
---
+++ src/common/utils/utils.h
@@ -0,0 +1,8 @@
+//
+// Created by tom on 26.05.2021.
+//
+
+#ifndef SIK_SCREEN_WORMS_UTILS_H
+#define SIK_SCREEN_WORMS_UTILS_H
+
+#endif //SIK_SCREEN_WORMS_UTILS_H
